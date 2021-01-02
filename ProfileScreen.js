61a1827b38bbae9675beca643d0e47ce894a0ba3
Marcin Wrzos <marwrz36@gmail.com>
2021-01-02 13:10:42 +0100

    input, description, usergui

--- ProfileScreen.js
+++ ProfileScreen.js
@@ -1,39 +1,93 @@
-import React from 'react';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState} from 'react';
 import {
   SafeAreaView,
+  StyleSheet,
+  ScrollView,
   View,
   Text,
+  StatusBar,
   Image,
   TouchableOpacity,
   TextInput,
+  Keyboard,
+  KeyboardAvoidingView,
 } from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
 import ImageE from './assets/Unknown.jpeg';
 import styled from 'styled-components';
 
-const onUpgradePress = () => {
-  console.log('XDDDD');
-};
 const ProfileScreen: () => React$Node = () => {
+  const [description, setDescription] = useState('Someting cool here...');
+  const [currentText, setCurrentText] = useState('');
+  const [level, setLevel] = useState(1);
+
+  const onDescriptionChange = () => {
+    if (currentText.length < 3) {
+      return;
+    }
+    setDescription(currentText);
+    setCurrentText('');
+    Keyboard.dismiss();
+  };
+
+  const onUpgradePress = () => {
+    setLevel(level + 1);
+  };
+
   return (
-    <SafeAreaView>
-      <Container>
-        <ProfileNameText>{'Hello'}</ProfileNameText>
-        <ProfileImage resizeMode={'contain'} source={ImageE} />
-        <DescriptionNameText>{'Someting cool here...'}</DescriptionNameText>
-        <UpgradeButton onPress={onUpgradePress}>
-          <UpgradeButtonText>{'UPGRADE'}</UpgradeButtonText>
-        </UpgradeButton>
-        <DescriptionInput placeholder={'New Description'} />
-        <DescriptionButton onPress={onUpgradePress}>
-          <DescriptionButtonText>{'UPGRADE'}</DescriptionButtonText>
-        </DescriptionButton>
-      </Container>
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <SafeAreaView>
+        <Container>
+          <ContentWrapper>
+            <ProfileImageWrapper>
+              <ProfileImage resizeMode={'contain'} source={ImageE} />
+              <ProfileNameText>{'Jan Nowak'}</ProfileNameText>
+
+              <LevelIndicator>
+                <LevelIndicatorText>{level}</LevelIndicatorText>
+              </LevelIndicator>
+            </ProfileImageWrapper>
+            <DescriptionNameText>{description}</DescriptionNameText>
+            <UpgradeButton onPress={onUpgradePress}>
+              <UpgradeButtonText>{'UPGRADE'}</UpgradeButtonText>
+            </UpgradeButton>
+          </ContentWrapper>
+          <SetDescriptionWrapper>
+            <DescriptionInput
+              placeholder={'New Description'}
+              value={currentText}
+              onChangeText={setCurrentText}
+              onSubmitEditing={onDescriptionChange}
+            />
+            <DescriptionButton onPress={onDescriptionChange}>
+              <DescriptionButtonText>{'SET'}</DescriptionButtonText>
+            </DescriptionButton>
+          </SetDescriptionWrapper>
+        </Container>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 };
 
 const Container = styled(View)`
+  display: flex;
   background-color: #eeeeee;
   width: 100%;
   height: 100%;
@@ -61,6 +115,8 @@ const UpgradeButton = styled(TouchableOpacity)`
   padding: 8px;
   border-radius: 4px;
   margin-top: 20px;
+  justify-content: center;
+  align-items: center;
 `;
 
 const UpgradeButtonText = styled(Text)`
@@ -68,21 +124,52 @@ const UpgradeButtonText = styled(Text)`
 `;
 
 const DescriptionInput = styled(TextInput)`
-  width: 100%;
   height: 50px;
   background-color: #aaa;
   padding: 8px;
+  flex: 1;
 `;
 
 const DescriptionButton = styled(TouchableOpacity)`
   background-color: red;
   padding: 8px;
   border-radius: 4px;
-  margin-top: 20px;
+  width: 80px;
+  justify-content: center;
+  align-items: center;
 `;
 
 const DescriptionButtonText = styled(Text)`
   color: white;
 `;
 
+const SetDescriptionWrapper = styled(View)`
+  display: flex;
+  flex-direction: row;
+  margin-top: 10px;
+  height: 50px;
+`;
+
+const LevelIndicator = styled(View)`
+  background-color: red;
+  width: 20px;
+  height: 20px;
+  border-radius: 10px;
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
+const LevelIndicatorText = styled(Text)`
+  color: white;
+  font-size: 12px;
+  text-align: center;
+`;
+
+const ProfileImageWrapper = styled(View)``;
+
+const ContentWrapper = styled(View)`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
 export default ProfileScreen;
